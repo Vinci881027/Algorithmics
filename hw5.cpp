a983c2e45166bc5f17513390a3122f774d809bc8
謝文棋 <xiedavid881027@gmail.com>
2020-05-23 15:51:45 +0800

    commit

--- hw5.cpp
+++ hw5.cpp
@@ -2,14 +2,35 @@
 #include <vector>
 using namespace std;
 
-void DFS(vector<int> adj[], bool visit[], int i, bool connected)
+vector<int> circuit;
+vector<int> rCircuit;
+
+void DFS(vector<int> adj[], bool visit[], int i)
 {
     for(int j = 0; j < adj[i].size(); j++){
-        if(!visit[adj[i][j]-1]){
-            visit[adj[i][j]-1] = true;
-            if(connected)
-                cout << adj[i][j] << " ";
-            DFS(adj, visit, adj[i][j]-1, connected);
+        if(!visit[adj[i][j]]){
+            visit[adj[i][j]] = true;
+            DFS(adj, visit, adj[i][j]);
+        }
+    }
+}
+
+void EulerianCircuit(vector<int> adj[], vector<int> visit[], int i)
+{
+    bool visitV;
+    for(int j = 0; j < adj[i].size(); j++){
+        visitV = false;
+        for(int k = 0; k < visit[i].size(); k++){
+            if(visit[i][k] == adj[i][j])
+                visitV = true;
+        }
+        if(!visitV){
+            visit[i].push_back(adj[i][j]);
+            visit[adj[i][j]].push_back(i);
+            circuit.push_back(i);
+            EulerianCircuit(adj, visit, adj[i][j]);
+            rCircuit.push_back(circuit.back());
+            circuit.pop_back();
         }
     }
 }
@@ -19,43 +40,41 @@ int main()
     int V, E;
     cin >> V >> E;
     int v1, v2;
-    int oddDegree = 0;
-    bool visit[V];
-    bool connected = true;
-    vector<int> adj[V];
+    bool evenDegree = true;
+    bool connect = true;
+    bool DFSvisit[V+1];
+    vector<int> adj[V+1];
+    vector<int> visit[V+1];
     for(int i = 0; i < E; i++){
         cin >> v1 >> v2;
-        adj[v1-1].push_back(v2);
-        adj[v2-1].push_back(v1);
+        adj[v1].push_back(v2);
+        adj[v2].push_back(v1);
     }
-    //count degree of every vertex
-    for(int i = 0; i < V; i++){
+    for(int i = 1; i <= V; i++){
         if(adj[i].size() % 2)
-            oddDegree++;
+            evenDegree = false;
+        DFSvisit[i] = false;
     }
-    if(oddDegree == 0 || oddDegree == 2){
-        for(int i = 0; i < V; i++)
-            visit[i] = false;
-        for(int i = 0; i < V; i++){
-            if(!visit[i]){
-                visit[i] = true;
-                DFS(adj, visit, i, false);
-            }
+    if(evenDegree){
+        DFS(adj, DFSvisit, 1);
+        for(int i = 1; i <= V; i++){
+            if(DFSvisit[i] == false)
+                connect = false;
         }
-        for(int i = 0; i < V; i++){
-            if(!visit[i])
-                connected = false;
-        }
-        for(int i = 0; i < V; i++)
-            visit[i] = false;
-        for(int i = 0; i < V; i++){
-            if(!visit[i]){
-                visit[i] = true;
-                DFS(adj, visit, i, connected);
+        if(connect){
+            EulerianCircuit(adj, visit, 1);
+            while(circuit.size() > 0){
+                rCircuit.push_back(circuit.back());
+                circuit.pop_back();
             }
+            while(rCircuit.size() > 0){
+                cout << rCircuit.back() << " ";
+                rCircuit.pop_back();
+            }
+            cout << "1 ";
+            return 0;
         }
     }
-    else
-        cout << "not exist" << endl;
+    cout << "not exist" << endl;
     return 0;
 }
